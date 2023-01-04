a1e11782d96e61cbd09a7038453d13db65dedf10
luigina <l.costante1@studenti.unisa.it>
2023-01-04 17:31:09 +0100

    [IS] Implementazione eccezioni package recensioni.application.exceptions

--- app/src/main/java/it/unisa/ilike/recensioni/application/exceptions/InvalidMotivazioneException.java
+++ app/src/main/java/it/unisa/ilike/recensioni/application/exceptions/InvalidMotivazioneException.java
@@ -1,4 +1,20 @@
 package it.unisa.ilike.recensioni.application.exceptions;
 
-public class InvalidMotivazioneException {
+/**
+ * Classe che estende RuntimeException
+ *  @version 0.1
+ *  @author LuiginaCostante
+ *  @see java.lang.RuntimeException
+ */
+
+public class InvalidMotivazioneException extends RuntimeException{
+
+    /**
+     * Questo metodo serve per lanciare una nuova eccezione di tipo InvalidMotivazioneException.
+     * Specifica che la motivazione relativa alla segnalazione da inserire contiene un numero di caratteri
+     * maggiore di 500 per cui non può essere considerata valida
+     */
+    public InvalidMotivazioneException(){
+        super ("La motivazione contiene un numero di caratteri maggiore di 500");
+    }
 }
