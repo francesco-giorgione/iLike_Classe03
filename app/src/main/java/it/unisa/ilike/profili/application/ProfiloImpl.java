bb896e181d9c458187ea235a82a472401fb1b284
NapMarta <marta.napolillo@outlook.it>
2023-01-04 23:29:07 +0100

    [IS] Aggiunta metodi ProfiloImpl e ProfiloService

--- app/src/main/java/it/unisa/ilike/profili/application/ProfiloImpl.java
+++ app/src/main/java/it/unisa/ilike/profili/application/ProfiloImpl.java
@@ -1,4 +1,19 @@
 package it.unisa.ilike.profili.application;
 
-public class ProfiloImpl {
+/**
+ * Questa classe rapresenta l'implentazione che gestisce le informazioni relative al profilo di un iscritto.
+ * @author Marta
+ * @version 0.1
+ */
+public class ProfiloImpl implements ProfiloService{
+
+    /**
+     * Questo metodo consente di restituire un oggetto Iscritto della tipologia IscrittoReal,
+     * cioè l'oggetto contenente anche le recensioni e la fato profilo dell'iscritto.
+     * @param i rappresenta l'oggetto IscrittoBean da cui recuperare tutte le sue informazioni
+     * @return l'oggetto IscrittoReal
+     */
+    public IscrittoBean getIscrittoReal(IscrittoBean i){
+
+    }
 }
