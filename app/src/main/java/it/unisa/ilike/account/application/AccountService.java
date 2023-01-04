9728556d1382103764fad5eff7e2646ee625404f
NapMarta <marta.napolillo@outlook.it>
2023-01-04 23:21:49 +0100

    [IS] Aggiunta metodi AccountImpl e AccountService

--- app/src/main/java/it/unisa/ilike/account/application/AccountService.java
+++ app/src/main/java/it/unisa/ilike/account/application/AccountService.java
@@ -1,4 +1,16 @@
 package it.unisa.ilike.account.application;
 
+/**
+ * Interfaccia esplicita i metodi di servizio per effettuare operazioni di autenticazione.
+ * @author Marta
+ * @version 0.1
+ */
 public interface AccountService {
+
+    public void login(String email, String password);
+    public IscrittoBean getIscritto();
+    public GestoreBean getGestore();
+    public void logout(Utente u);
+    public IscrittoBean registrazioneIscritto(String email, String password, String nome, String cognome, String nickname, String bio, Blob foto);
+
 }
