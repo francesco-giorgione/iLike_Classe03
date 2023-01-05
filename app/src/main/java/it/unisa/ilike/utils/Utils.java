a6d8cc19b95dcfdc08bb0a7655989de0f8675c2c
francesco-giorgione <francesco.giorgione01@gmail.com>
2023-01-05 16:29:43 +0100

    Aggiunta metodo fittizio hasLista

--- app/src/main/java/it/unisa/ilike/utils/Utils.java
+++ app/src/main/java/it/unisa/ilike/utils/Utils.java
@@ -1,9 +1,16 @@
 package it.unisa.ilike.utils;
 
+import it.unisa.ilike.account.storage.IscrittoBean;
 import it.unisa.ilike.account.storage.UtenteBean;
 
 public class Utils {
+    // manca reale implementazione
     public static Boolean isIscritto(UtenteBean utente) {
         return true;
+    }
+
+    // manca reale implementazione
+    public static Boolean hasLista(IscrittoBean i, String nome) {
+        return false;
     }
 }
