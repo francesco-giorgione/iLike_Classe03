a04ac66e94083bd5de414244535817a754617f1a
francesco-giorgione <francesco.giorgione01@gmail.com>
2023-01-05 12:01:23 +0100

    [IS] Implementazione classi e interfacce del package contenuti.application (i metodi di ContenutoImpl non sono stati implementati nell'attesa della disponibilità dei DAO)

--- app/src/main/java/it/unisa/ilike/contenuti/application/ContenutoService.java
+++ app/src/main/java/it/unisa/ilike/contenuti/application/ContenutoService.java
@@ -1,4 +1,29 @@
 package it.unisa.ilike.contenuti.application;
 
+import java.util.List;
+
+import it.unisa.ilike.contenuti.application.exceptions.InvalidValutazioneException;
+import it.unisa.ilike.contenuti.storage.ContenutoBean;
+
+/**
+ * L'interfaccia espone i servizi relativi alla gestione dei contenuti.
+ *  @version 0.1
+ *  @author FrancescoGiorgione
+ */
 public interface ContenutoService {
+    /**
+     * @param categoria è la categoria dei contenuti che si vogliono selezionare
+     * @return una collezione List dei contenuti della categoria indicata
+     */
+    List<ContenutoBean> getContenuti(String categoria);
+
+    /**
+     *
+     * @param minValutazione è la minima valutazione media che devono avere i contenuti che si vogliono selezionare
+     * @param maxValutazione è la massima valutazione media che devono avere i contenuti che si vogliono selezionare
+     * @return una collezione List dei contenuti aventi una valutazione media che rientra nel range indicato
+     * @throws InvalidValutazioneException
+     */
+    List<ContenutoBean> getContenuti(Double minValutazione, Double maxValutazione)
+            throws InvalidValutazioneException;
 }
