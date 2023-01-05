2ca47c06936d4c9e6be943de7711af2cbf2fe605
francesco-giorgione <francesco.giorgione01@gmail.com>
2023-01-05 18:53:25 +0100

    Implementazione metodi della classe ListaImpl

--- app/src/main/java/it/unisa/ilike/liste/application/ListaImpl.java
+++ app/src/main/java/it/unisa/ilike/liste/application/ListaImpl.java
@@ -1,5 +1,6 @@
 package it.unisa.ilike.liste.application;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import it.unisa.ilike.account.storage.IscrittoBean;
@@ -9,6 +10,8 @@ import it.unisa.ilike.liste.application.exceptions.InvalidNomeException;
 import it.unisa.ilike.liste.application.exceptions.ListaGiaEsistenteException;
 import it.unisa.ilike.liste.application.exceptions.NomeVuotoException;
 import it.unisa.ilike.liste.storage.ListaBean;
+import it.unisa.ilike.liste.storage.ListaDAO;
+import it.unisa.ilike.utils.Utils;
 import it.unisa.ilike.utils.exceptions.NotIscrittoException;
 
 /**
@@ -23,18 +26,42 @@ public class ListaImpl implements ListaService {
     public boolean creaLista(IscrittoBean i, String nome, boolean pubblica) throws
             NotIscrittoException, NomeVuotoException, InvalidNomeException, ListaGiaEsistenteException {
 
-        return false;
+        if(!Utils.isIscritto(i))        { throw new NotIscrittoException(); }
+        if(nome.length() == 0)          { throw new NomeVuotoException(); }
+        if(nome.length() > 50)          { throw new InvalidNomeException(); }
+        if(Utils.hasLista(i, nome))     { throw new ListaGiaEsistenteException(); }
+
+        ListaBean lista = new ListaBean(nome, i.getEmail(), pubblica);
+        lista.setContenuti(new ArrayList<>());
+        ListaDAO dao = new ListaDAO();
+
+        return dao.doSave(lista);
     }
 
     /** @inheritDoc */
     @Override
     public boolean aggiungiContenuto(ListaBean l, ContenutoBean c) throws ContenutoGiaPresenteException {
-        return false;
+        for(ContenutoBean curr: l.getContenuti()) {
+            if(curr.getId() == c.getId()) {
+                throw new ContenutoGiaPresenteException();
+            }
+        }
+
+        l.aggiungiContenuto(c);
+        ListaDAO dao = new ListaDAO();
+
+        if(dao.doUpdate(l)) {
+            return true;
+        }
+        else {
+            l.getContenuti().remove(l.getContenuti().size() - 1);
+            return false;
+        }
     }
 
     /** @inheritDoc */
     @Override
     public List<ContenutoBean> getContenutiLista(ListaBean l) {
-        return null;
+        return l.getContenuti();
     }
 }
