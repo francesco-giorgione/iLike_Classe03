19db08cb55d2afa77f4c609a8990718ee49a7451
luigina <l.costante1@studenti.unisa.it>
2023-01-06 18:17:38 +0100

    [IS] interfaccia grafica visualizzazione_segnalazioni

--- app/src/main/java/it/unisa/ilike/account/presentation/LoginActivity.java
+++ app/src/main/java/it/unisa/ilike/account/presentation/LoginActivity.java
@@ -1,16 +1,7 @@
 package it.unisa.ilike.account.presentation;
 
 import android.app.Activity;
-
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
-
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
@@ -21,6 +12,12 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import it.unisa.ilike.account.presentation.R;
 import it.unisa.ilike.account.presentation.databinding.ActivityLogin2Binding;
--- app/src/main/java/it/unisa/ilike/segnalazioni/presentation/CancellazioneRecensioneActivity.java
+++ app/src/main/java/it/unisa/ilike/segnalazioni/presentation/CancellazioneRecensioneActivity.java
@@ -11,6 +11,6 @@ public class CancellazioneRecensioneActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cancellazione_recensione);
+        setContentView(R.layout.activity_gestione_segnalazione);
     }
 }
