40711c6e325761d569abd44db783e1304ddd8dfe
luigina <l.costante1@studenti.unisa.it>
2023-01-07 13:46:14 +0100

    [IS] collegamenti activity CreazioneListaActivity.java

--- app/src/main/java/it/unisa/ilike/liste/presentation/CreazioneListaActivity.java
+++ app/src/main/java/it/unisa/ilike/liste/presentation/CreazioneListaActivity.java
@@ -1,10 +1,14 @@
 package it.unisa.ilike.liste.presentation;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import it.unisa.ilike.R;
+import it.unisa.ilike.contenuti.presentation.VisualizzazioneHomepageActivity;
+import it.unisa.ilike.profili.presentation.VisualizzazioneProfiloPersonaleActivity;
 
 public class CreazioneListaActivity extends AppCompatActivity {
 
@@ -12,5 +16,25 @@ public class CreazioneListaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creazione_lista);
+    }
+
+    public void onClickProfilo(View v){
+        Intent i = new Intent();
+        i.setClass(getApplicationContext(), VisualizzazioneProfiloPersonaleActivity.class);
+        startActivity(i);
+    }
+
+    public void onClickHomepage(View v){
+        Intent i = new Intent();
+        i.setClass(getApplicationContext(), VisualizzazioneHomepageActivity.class);
+        startActivity(i);
+    }
+
+    //da finire (implementato solo il collegamento di ritorno)
+    public void creaLista(View v){
+        Intent i = new Intent();
+        i.setClass(getApplicationContext(), VisualizzazioneProfiloPersonaleActivity.class);
+        startActivity(i);
+        //Toast.makeText(getApplicationContext(), "Lista creata", Toast.LENGTH_LONG).show();
     }
 }
