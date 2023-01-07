96e64b32c6c382fc1c8883a0a71055691d37b570
francesco-giorgione <francesco.giorgione01@gmail.com>
2023-01-07 13:33:29 +0100

    Bug fix

--- app/src/main/java/it/unisa/ilike/contenuti/storage/AlbumMusicaleDAO.java
+++ app/src/main/java/it/unisa/ilike/contenuti/storage/AlbumMusicaleDAO.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import it.unisa.ilike.QueryManager;
+import it.unisa.ilike.utils.Utils;
 
 public class AlbumMusicaleDAO extends ContenutoDAO {
 
