01211cf291bd993c697096a2c258515e2f0c6e4c
NapMarta <marta.napolillo@outlook.it>
2023-01-07 10:49:07 +0100

    [IS] Aggiunta eccezioni NotIscrittoException

--- app/src/main/java/it/unisa/ilike/profili/application/ProfiloImpl.java
+++ app/src/main/java/it/unisa/ilike/profili/application/ProfiloImpl.java
@@ -1,13 +1,16 @@
 package it.unisa.ilike.profili.application;
 
+import static it.unisa.ilike.utils.Utils.isIscritto;
+
 import it.unisa.ilike.account.storage.IscrittoBean;
 import it.unisa.ilike.profili.storage.IscrittoProxyBean;
 import it.unisa.ilike.profili.storage.IscrittoRealBean;
+import it.unisa.ilike.utils.exceptions.NotIscrittoException;
 
 /**
  * Questa classe rappresenta l'implentazione che gestisce le informazioni relative al profilo di un iscritto.
  * @author Marta
- * @version 0.1
+ * @version 0.2
  */
 public class ProfiloImpl implements ProfiloService{
 
@@ -17,14 +20,16 @@ public class ProfiloImpl implements ProfiloService{
      * @param i rappresenta l'oggetto IscrittoBean da cui recuperare tutte le sue informazioni
      * @return l'oggetto IscrittoReal
      */
-    public IscrittoBean getIscrittoReal(IscrittoBean i){
-        if (i instanceof IscrittoProxyBean){
-            IscrittoRealBean iscrittoReal =  new IscrittoRealBean(i.getEmail(), i.getPassword(), i.getNickname(), i.getNome(), i.getCognome(), i.getBio());
-            iscrittoReal.setRecensioni(i.getRecensioni());
-            iscrittoReal.setListe(i.getListe());
-            iscrittoReal.setFoto(i.getFoto());
-            return iscrittoReal;
-        }
-        else return i;
+    public IscrittoBean getIscrittoReal(IscrittoBean i) throws NotIscrittoException {
+        if (isIscritto(i)){
+            if (i instanceof IscrittoProxyBean){
+                IscrittoRealBean iscrittoReal =  new IscrittoRealBean(i.getEmail(), i.getPassword(), i.getNickname(), i.getNome(), i.getCognome(), i.getBio());
+                iscrittoReal.setRecensioni(i.getRecensioni());
+                iscrittoReal.setListe(i.getListe());
+                iscrittoReal.setFoto(i.getFoto());
+                return iscrittoReal;
+            }
+            else return i;
+        }else throw new NotIscrittoException();
     }
 }
