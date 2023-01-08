22e438e81c46ab59ce9ce081aedef0a6e1182d28
francesco-giorgione <francesco.giorgione01@gmail.com>
2023-01-08 18:59:44 +0100

    Modifica metodo doRetrieveByKey

--- app/src/main/java/it/unisa/ilike/liste/storage/ListaDAO.java
+++ app/src/main/java/it/unisa/ilike/liste/storage/ListaDAO.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import it.unisa.ilike.QueryManager;
+import it.unisa.ilike.account.storage.IscrittoDAO;
 import it.unisa.ilike.contenuti.storage.AlbumMusicaleBean;
 import it.unisa.ilike.contenuti.storage.AlbumMusicaleDAO;
 import it.unisa.ilike.contenuti.storage.ContenutoBean;
@@ -28,6 +29,22 @@ import it.unisa.ilike.utils.Utils;
  */
 
 public class ListaDAO {
+    /**
+     * Modella il risultato della prima query eseguita in doRetrieveByKey. Consente cioè
+     * di convertire il risultato dal formato json in un oggetto Java.
+     */
+    public class RisultatoQuery {
+        String nome, emailIscritto;
+        int visibilita;
+    }
+
+    /**
+     * Modella il risultato della seconda query eseguita in doRetrieveByKey. Consente cioè
+     * di convertire il risultato dal formato json in un oggetto Java.
+     */
+    public class RisultatoQuery2 {
+        int idContenuto;
+    }
 
     /**
      * Questo metodo consente di salvare nella tabella Lista del database un nuovo oggetto della classe
@@ -65,6 +82,28 @@ public class ListaDAO {
         else return false;
     }
 
+    /**
+     * Questo metodo consente di cancellare un oggetto <code>ListaBean</code> dalla tabella Lista del database,
+     * individuandolo tramite l'id passato come argomento.
+     * @param nome è il nome della lista da cancellare dal database
+     * @param emailIscritto è l'email dell'iscritto cui appartiene la lista da cancellare dal database
+     * @return true se l'operazione è andata a buon fine, false altrimenti
+     */
+    private boolean doDeleteByKey(String nome, String emailIscritto){
+        if (nome == null || emailIscritto == null){
+            return false;
+        }
+
+        nome = addEscape(nome);
+        emailIscritto = addEscape(emailIscritto);
+
+        QueryManager queryManager= new QueryManager();
+
+        String query = "delete from Liste where nome = '" + nome + "' and email_iscritto = '" + emailIscritto + "'";
+
+        return queryManager.update(query);
+    }
+
 
     /**
      * Restituisce un oggetto della classe <code>ListaBean</code>
@@ -80,38 +119,61 @@ public class ListaDAO {
             return null;
         }
 
-        nome = addEscape(nome);
-        emailIscritto = addEscape(emailIscritto);
+        nome = Utils.addEscape(nome);
+        emailIscritto = Utils.addEscape(emailIscritto);
 
-         String query = "select L.visibilita as visibilita, L.nome as nome, L.email_iscritto as emailIscritto, I.id_contenuto " +
-                "from Liste L join Inclusioni I on I.nome_lista = L.nome and I.email_iscritto = L.email_iscritto " +
-                "where L.nome = '" + nome + "' and L.email_iscritto = '" + emailIscritto + "'";
+        String query = "select nome, email_iscritto as emailIscritto, visibilita " +
+                "from Liste " +
+                "where nome = '" + nome + "' and email_iscritto = '" + emailIscritto + "'";
 
-        QueryManager queryManager= new QueryManager();
-        String res = queryManager.select(query);
-        Gson gson= new Gson();
-        ListaBean lista = gson.fromJson(res, ListaBean.class);
+        Gson gson = new Gson();
+        QueryManager queryManager = new QueryManager();
+        String jsonRes = queryManager.select(query);
+        RisultatoQuery[] res = gson.fromJson(jsonRes, RisultatoQuery[].class);
+
+        if(res.length == 0) {
+            return null;
+        }
+
+        RisultatoQuery tmpRes = res[0];
+        ListaBean lista = new ListaBean();
+        lista.setNome(nome);
+        lista.setVisibilita(tmpRes.visibilita == 1);
         lista.setContenuti(new ArrayList<>());
+        lista.setIscritto(new IscrittoDAO().doRetrieveByEmail(tmpRes.emailIscritto));
 
-        for(ContenutoBean c: lista.getContenuti()) {
-            int id = c.getId();
+        query = "select id_contenuto as idContenuto " +
+                "from Inclusioni " +
+                "where nome_lista = '" + nome + "' and email_iscritto = '" + emailIscritto + "'";
+
+        jsonRes = queryManager.select(query);
+        RisultatoQuery2[] res2 = gson.fromJson(jsonRes, RisultatoQuery2[].class);
+
+        if(res2.length == 0) {
+            return lista;
+        }
+
+        for(RisultatoQuery2 curr: res2) {
+            int id = curr.idContenuto;
+            ContenutoBean currBean = new ContenutoDAO().doRetrieveById(curr.idContenuto);
             ContenutoBean toAdd = null;
 
-            if(c instanceof FilmBean) {
+            toAdd = new LibroDAO().doRetrieveById(id);
+
+            if(toAdd == null) {
                 toAdd = new FilmDAO().doRetrieveById(id);
-            }
-            else if(c instanceof SerieTVBean) {
-                toAdd = new SerieTVDAO().doRetrieveById(id);
-            }
-            else if(c instanceof LibroBean) {
-                toAdd = new LibroDAO().doRetrieveById(id);
-            }
-            else if(c instanceof AlbumMusicaleBean) {
-                toAdd = new AlbumMusicaleDAO().doRetrieveById(id);
+
+                if(toAdd == null) {
+                    toAdd = new SerieTVDAO().doRetrieveById(id);
+
+                    if(toAdd == null) {
+                        toAdd = new AlbumMusicaleDAO().doRetrieveById(id);
+                    }
+                }
             }
 
             if(toAdd != null) {
-                lista.getContenuti().add(toAdd);
+                lista.aggiungiContenuto(toAdd);
             }
         }
 
