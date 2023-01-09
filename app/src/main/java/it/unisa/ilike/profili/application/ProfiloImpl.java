f8a420b3426fa8ff489c7d79574380a24def1c4f
francesco-giorgione <francesco.giorgione01@gmail.com>
2023-01-09 17:03:15 +0100

    Bug fix

--- app/src/main/java/it/unisa/ilike/profili/application/ProfiloImpl.java
+++ app/src/main/java/it/unisa/ilike/profili/application/ProfiloImpl.java
@@ -1,11 +1,8 @@
 package it.unisa.ilike.profili.application;
 
-import static it.unisa.ilike.utils.Utils.isIscritto;
-
 import it.unisa.ilike.account.storage.IscrittoBean;
 import it.unisa.ilike.profili.storage.IscrittoProxyBean;
 import it.unisa.ilike.profili.storage.IscrittoRealBean;
-import it.unisa.ilike.utils.exceptions.NotIscrittoException;
 
 /**
  * Questa classe rappresenta l'implentazione che gestisce le informazioni relative al profilo di un iscritto.
@@ -20,16 +17,18 @@ public class ProfiloImpl implements ProfiloService{
      * @param i rappresenta l'oggetto IscrittoBean da cui recuperare tutte le sue informazioni
      * @return l'oggetto IscrittoReal
      */
-    public IscrittoBean getIscrittoReal(IscrittoBean i) throws NotIscrittoException {
-        if (isIscritto(i)){
-            if (i instanceof IscrittoProxyBean){
-                IscrittoRealBean iscrittoReal =  new IscrittoRealBean(i.getEmail(), i.getPassword(), i.getNickname(), i.getNome(), i.getCognome(), i.getBio());
-                iscrittoReal.setRecensioni(i.getRecensioni());
-                iscrittoReal.setListe(i.getListe());
-                iscrittoReal.setFoto(i.getFoto());
-                return iscrittoReal;
-            }
-            else return i;
-        }else throw new NotIscrittoException();
+    public IscrittoBean getIscrittoReal(IscrittoBean i) {
+        if(i == null) {
+            return null;
+        }
+
+        if (i instanceof IscrittoProxyBean){
+            IscrittoRealBean iscrittoReal =  new IscrittoRealBean(i.getEmail(), i.getPassword(), i.getNickname(), i.getNome(), i.getCognome(), i.getBio());
+            iscrittoReal.setRecensioni(i.getRecensioni());
+            iscrittoReal.setListe(i.getListe());
+            iscrittoReal.setFoto(i.getFoto());
+            return iscrittoReal;
+        }
+        else return i;
     }
 }
