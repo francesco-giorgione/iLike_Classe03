f3abe7049d7d5cec95e0b2fc80460ad979c5a249
francesco-giorgione <francesco.giorgione01@gmail.com>
2023-01-09 13:33:13 +0100

    Rimozione NotGestoreException e NotIscrittoException

--- app/src/main/java/it/unisa/ilike/utils/exceptions/NotGestoreException.java
+++
@@ -1,4 +0,0 @@
-package it.unisa.ilike.utils.exceptions;
-
-public class NotGestoreException extends Exception {
-}
--- app/src/main/java/it/unisa/ilike/utils/exceptions/NotIscrittoException.java
+++
@@ -1,4 +0,0 @@
-package it.unisa.ilike.utils.exceptions;
-
-public class NotIscrittoException extends Exception {
-}
