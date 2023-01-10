7671042222ac5a0cf233c818cdf1c1f61501f2ef
luigina <l.costante1@studenti.unisa.it>
2023-01-10 19:23:29 +0100

    [IS] Aggiunta classe interna GsonResultSegnalazioni in VisualizzazioneSegnalazioniActivity.java e relativi metodi. Inizializzazione adapter in VisualizzazioneSegnalazioniActivity.java

--- app/src/main/java/it/unisa/ilike/segnalazioni/application/activities/VisualizzazioneSegnalazioniActivity.java
+++ app/src/main/java/it/unisa/ilike/segnalazioni/application/activities/VisualizzazioneSegnalazioniActivity.java
@@ -1,22 +1,58 @@
 package it.unisa.ilike.segnalazioni.application.activities;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import it.unisa.ilike.R;
 import it.unisa.ilike.account.application.AccountImpl;
 import it.unisa.ilike.account.application.AccountService;
 import it.unisa.ilike.account.storage.Account;
-import it.unisa.ilike.account.storage.GestoreBean;
 import it.unisa.ilike.contenuti.application.activities.VisualizzazioneHomepageActivity;
+import it.unisa.ilike.segnalazioni.application.SegnalazioneImpl;
+import it.unisa.ilike.segnalazioni.application.SegnalazioneService;
+import it.unisa.ilike.segnalazioni.application.VisualizzazioneSegnalazioniAdapter;
+import it.unisa.ilike.segnalazioni.storage.SegnalazioneBean;
 
 public class VisualizzazioneSegnalazioniActivity extends AppCompatActivity {
+    VisualizzazioneSegnalazioniAdapter adapter;
+
+
+    private class GsonResultSegnalazioni extends AsyncTask<Void, Void, ArrayList<SegnalazioneBean>> {
+
+        @Override
+        protected ArrayList<SegnalazioneBean> doInBackground(Void... v) {
+            SegnalazioneService s= new SegnalazioneImpl();
+            ArrayList<SegnalazioneBean> segnalazioni= (ArrayList<SegnalazioneBean>) s.getSegnalazione();
+            return segnalazioni;
+        }
+
+
+        @Override
+        protected void onPostExecute(ArrayList<SegnalazioneBean> segnalazioni) {
+            Log.d("MyDebug", "onPostExecute visualizzazione segnalazioni act");
+
+            if (segnalazioni.size()>0) {
+                for (SegnalazioneBean s : segnalazioni)
+                    adapter.add(s);
+            }
+            else {
+                Log.d("MyDebug", "nessuna segnalazione trovata");
+                Toast.makeText(getApplicationContext(), "Nessuna segnalazione trovata", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +61,22 @@ public class VisualizzazioneSegnalazioniActivity extends AppCompatActivity {
 
         Intent i = getIntent();
         setReturnIntent();
-        account = (Account) getIntent().getExtras().getSerializable("account");
-        GestoreBean gestoreBean = account.getGestoreBean();
-        TextView numerSegnalazioniTextView = (TextView) findViewById(R.id.numerSegnalazioniGestite);
-        numerSegnalazioniTextView.setText(gestoreBean.getNumSegnalazioniGestite());
+        //account = (Account) getIntent().getExtras().getSerializable("account");
+        //GestoreBean gestoreBean = account.getGestoreBean();
+        //TextView numerSegnalazioniTextView = (TextView) findViewById(R.id.numerSegnalazioniGestite);
+        //numerSegnalazioniTextView.setText(gestoreBean.getNumSegnalazioniGestite());
+        ListView segnalazioniList= findViewById(R.id.segnalazioniList);
+
+        VisualizzazioneSegnalazioniAdapter adapter= new VisualizzazioneSegnalazioniAdapter(this,
+                R.layout.activity_list_element_visualizzazione_segnalazioni,
+                new ArrayList<SegnalazioneBean>());
+        segnalazioniList.setAdapter(adapter);
+        GsonResultSegnalazioni g= (GsonResultSegnalazioni) new GsonResultSegnalazioni().execute(new Void[0]);
     }
 
     private void setReturnIntent() {
         Intent data = new Intent();
         setResult(RESULT_OK,data);
-    }
-
-    public void onClickVisualizzaSegnalazioni(View view) {
-        Intent i = new Intent();
-        i.setClass(getApplicationContext(), VisualizzazioneSegnalazioniActivity.class);
-        i.putExtra("account", (Serializable) account);
-        startActivity(i);
     }
 
     public void onClickInfo(View view){
