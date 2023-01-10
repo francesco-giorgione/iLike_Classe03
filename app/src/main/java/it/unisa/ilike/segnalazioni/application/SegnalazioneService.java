334197c9a8d7ead367f6d7f95e221b9149c6daa8
francesco-giorgione <francesco.giorgione01@gmail.com>
2023-01-10 11:53:27 +0100

    Modifica javadoc

--- app/src/main/java/it/unisa/ilike/segnalazioni/application/SegnalazioneService.java
+++ app/src/main/java/it/unisa/ilike/segnalazioni/application/SegnalazioneService.java
@@ -12,10 +12,36 @@ import it.unisa.ilike.segnalazioni.storage.SegnalazioneBean;
  */
 
 public interface SegnalazioneService {
-
+    /**
+     * Questo metodo consente al gestore di visualizzare tutte le segnalazioni che ancora non sono state gestite.
+     * @return lista di segnalazioni non gestite
+     */
     public List<SegnalazioneBean> getSegnalazione();
+
+
+    /**
+     * Questo metodo può essere chiamato all’interno dell’applicazione solo da chi dispone di un account gestore e
+     * consente di cancellare una recensione in seguito alla ricezione di una o più segnalazioni. Purché ciò avvenga
+     * occorre fornire al metodo la segnalazione relativa alla recensione da cancellare ed una stringa contenente
+     * la motivazione della cancellazione. Il metodo incrementa inoltre il numero di segnalazioni gestite dal gestore.
+     * @param s segnalazione relativa alla recensione da cancellare
+     * @param motivazione testo che motiva la cancellazione della recensione
+     * @param g gestore che si occupa della cancellazione della recensione
+     * @return valore booleano che descrive l'esito dell'operazione
+     * @throws it.unisa.ilike.segnalazioni.application.exceptions.MotivazioneVuotaException
+     * @throws it.unisa.ilike.segnalazioni.application.exceptions.InvalidMotivazioneException
+     */
     public Boolean cancellaRecensione(SegnalazioneBean s, String motivazione, GestoreBean g)
             throws it.unisa.ilike.segnalazioni.application.exceptions.MotivazioneVuotaException, it.unisa.ilike.segnalazioni.application.exceptions.InvalidMotivazioneException;
+
+
+    /**
+     * Questo metodo permette al gestore di ignorare una segnalazione, rifiutandola in quanto non veritiera.
+     * Inoltre, il metodo incrementa il numero di segnalazioni gestite dal gestore.
+     * @param s segnalazione da rifiutare
+     * @param g gestore che si occupa di rifiutare la segnalazione
+     * @return valore booleano che descrive l'esito dell'operazione
+     */
     public Boolean rifiutaSegnalazione(SegnalazioneBean s, GestoreBean g);
 
 }
