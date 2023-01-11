fb7418bd19f34e7788db6cfc204f6ec1031ab1f2
luigina <l.costante1@studenti.unisa.it>
2023-01-11 17:39:29 +0100

    [IS] modifiche varie visualizzazione dettagliata contenuto (adapter e grafica)

--- app/src/main/java/it/unisa/ilike/contenuti/application/VisualizzazioneDettagliataContenutoAdapter.java
+++ app/src/main/java/it/unisa/ilike/contenuti/application/VisualizzazioneDettagliataContenutoAdapter.java
@@ -6,17 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
 import it.unisa.ilike.R;
 import it.unisa.ilike.account.storage.IscrittoBean;
-import it.unisa.ilike.contenuti.storage.ContenutoBean;
-import it.unisa.ilike.contenuti.storage.FilmBean;
-import it.unisa.ilike.contenuti.storage.LibroBean;
-import it.unisa.ilike.contenuti.storage.SerieTVBean;
 import it.unisa.ilike.recensioni.storage.RecensioneBean;
 
 public class VisualizzazioneDettagliataContenutoAdapter extends ArrayAdapter<RecensioneBean> {
@@ -35,19 +30,9 @@ public class VisualizzazioneDettagliataContenutoAdapter extends ArrayAdapter<Rec
         }
 
         RecensioneBean r = getItem(position);
-        ContenutoBean c= r.getContenuto();
 
         Log.d("DEBUG","recensione r="+r);
 
-        ImageView icona= v.findViewById(R.id.icona);
-        if (c instanceof FilmBean)
-            icona.setImageDrawable(v.getResources().getDrawable(R.drawable.icona_film));
-        else if (c instanceof SerieTVBean)
-            icona.setImageDrawable(v.getResources().getDrawable(R.drawable.icona_serietv));
-        else if (c instanceof LibroBean)
-            icona.setImageDrawable(v.getResources().getDrawable(R.drawable.icona_libro));
-        else
-            icona.setImageDrawable(v.getResources().getDrawable(R.drawable.icona_musica));
 
         TextView nickname= v.findViewById(R.id.nickname);
         IscrittoBean i= r.getIscritto();
