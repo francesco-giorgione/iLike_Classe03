cd1d2d13a0f66ff35c5f82c9f558d1ad2b08b590
simoLoc <simonaloc1@yahoo.it>
2023-01-11 18:19:12 +0100

    [IS] Aggiunta gestione foto in visualizzazione profilo personale

--- app/src/main/java/it/unisa/ilike/account/application/activities/VisualizzazioneProfiloPersonaleActivity.java
+++ app/src/main/java/it/unisa/ilike/account/application/activities/VisualizzazioneProfiloPersonaleActivity.java
@@ -3,12 +3,16 @@ package it.unisa.ilike.account.application.activities;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.BufferedInputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -51,6 +55,14 @@ public class VisualizzazioneProfiloPersonaleActivity extends Activity {
         TextView infoTextView = findViewById(R.id.infoTextView);
         nicknameTextView.setText(iscritto.getNickname());
         infoTextView.setText(iscritto.getBio());
+
+        ImageView icona = findViewById(R.id.icona);
+
+        if(iscritto.getFoto() != null){
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(iscritto.getFoto());
+            Bitmap bitmap = BitmapFactory.decodeStream(bufferedInputStream);
+            icona.setImageBitmap(bitmap);
+        }
 
 
         //inizializzazione adapter liste profilo iscritto
