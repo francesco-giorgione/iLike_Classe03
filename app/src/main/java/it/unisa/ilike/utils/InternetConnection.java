3d7752730c8d38dd5b33ca22c5fab324f0a0d71c
luigina <l.costante1@studenti.unisa.it>
2023-01-14 11:54:01 +0100

    [IS] verifica connessione internet activities package contenuti

---
+++ app/src/main/java/it/unisa/ilike/utils/InternetConnection.java
@@ -0,0 +1,26 @@
+package it.unisa.ilike.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class InternetConnection {
+
+    public static boolean haveInternetConnection(Context contesto) {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) contesto.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+
+}
