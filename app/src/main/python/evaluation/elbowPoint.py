31ab4de7509a75b029f3ff283afe77bc87039031
luigina <l.costante1@studenti.unisa.it>
2023-01-24 14:02:42 +0100

    [FIA] modifiche documentazione e classe elbowPoint.py

--- app/src/main/python/evaluation/elbowPoint.py
+++ app/src/main/python/evaluation/elbowPoint.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.decomposition import PCA
 
-# Caricamento del dataset
+# Carica il dataset
 table = pd.read_csv('film.csv', sep=',')
 
 numericCol = ["erotismo", "tensione", "impegno", "ritmo", "humor", "voti_totali"]
@@ -13,16 +13,16 @@ pca = PCA()
 # Transformazione dei dati data
 df = pca.fit_transform(tableNum)
 
-# Inizializzazione di una lista per memorizzare i valori di inerzia
+# Inizializza una lista per memorizzare i valori di inerzia
 inertias = []
 
-#k-means su un numero crescente di cluster
-for k in range(1, 11):
-    kmeans = KMeans(n_clusters=k, random_state=0, n_init=10).fit(tableNum)
+# Esegui il k-means su un numero crescente di cluster
+for k in range(2, 25):
+    kmeans = KMeans(init='k-means++', algorithm='elkan', n_clusters=k, random_state=0, n_init='auto').fit(tableNum)
     inertias.append(kmeans.inertia_)
 
-# Disegno del grafico dei valori di inerzia
-plt.plot(range(1, 11), inertias, marker='o')
+# Disegna un grafico dei valori di inerzia
+plt.plot(range(2, 25), inertias, marker='o')
 plt.xlabel('Numero di cluster')
 plt.ylabel('Inerzia')
 plt.show()
